cfa28ead820e30d71793d233b83b6518dc2acbec
Sindhu5653 <sindhupvly@gmail.com>
2025-06-18 21:58:16 +0530

    Home Page

--- Portfolio Website/src/App.jsx
+++ Portfolio Website/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Frontpage from './components/Frontpage'
+import About from './components/About'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Frontpage/>
+      <About/>
+      <Projects/>
+      <Contact/>
+      {/* <Footer/> */}
     </div>
   )
 }
